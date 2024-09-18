255dee41370a4e1269ee93bad97786e8e147592c
Eva Montserrat <19002330@alumnos.utleon.edu.mx>
2024-09-17 19:35:18 -0600

    Commit del martes 17/09/2024 Ocultar imagen

---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl:'./product-list.component.html', 
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  title="Saludo de variable";
+
+  imageWidth:number=50;
+  imageMargi:number=2;
+  muestraImg:boolean=true;
+  listFilter:string='';
+
+  showImage():void{
+    this.muestraImg=!this.muestraImg;
+  }
+
+
+  productos:any[] = [
+    {
+
+      "producto":1,
+      "modelo": 'Sentra',
+      "descripcion":'4 puertas familiar',
+      "year":'febrero 3 2022',
+      "precio":200000,
+      "marca":'NISSAN',
+      "color":'morado',
+      "imagenUrl":"https://blogmedia.dealerfire.com/wp-content/uploads/sites/858/2019/11/Sentra-Reveal-2-source.jpg"
+    },
+    {
+
+      "producto":2,
+      "modelo": 'A4',
+      "descripcion":'2 puertas familiar',
+      "year":'febrero 3 2023',
+      "precio":200000,
+      "marca":'NISSAN',
+      "color":'morado',
+      "imagenUrl":"http://www.audi-sport.net/xf/attachments/inspiration-jpg.75443/"
+    },
+    {
+
+      "producto":3,
+      "modelo": 'Rio',
+      "descripcion":'2 puertas familiar',
+      "year":'febrero 3 2022',
+      "precio":200000,
+      "marca":'KIA',
+      "color":'Azul',
+      "imagenUrl":"https://tse2.mm.bing.net/th?id=OIP.A8wpf_PlilMC71zmOURwawHaE7&pid=Api"
+    }
+
+  ]
+}
